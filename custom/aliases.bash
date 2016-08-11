def9e98bca03635a9054009470b34481da2e6f9d
Aleš Culek <ales.culek@gmail.com>
2016-08-11 13:03:36 +0200

    Removed custom/ from ignoring

---
+++ custom/aliases.bash
@@ -0,0 +1,41 @@
+##
+## Common
+##
+
+alias cdp="cd ${HOME}/Projects"
+alias colibs="cd ${HOME}/composer/libs"
+
+phpunit() {
+  phpunit "$@"
+}
+alias pu=phpunit
+
+##
+## GIT
+##
+
+alias ggk="gitk --all &"
+alias ggui="git gui &"
+alias ggw="git add -A . && git commit -m \"WIP\" ."
+alias gguw="git reset HEAD~1"
+alias ggs="git status -u"
+
+gitDiff() {
+  git diff --color --minimal --patience "$@" | /usr/local/bin/diff-highlight | more
+}
+alias ggd=gitDiff
+
+gitRebaseInteractive() {
+  git rebase -i "$@"
+}
+alias ggr=gitRebaseInteractive
+
+# Processes management
+psgFunction() {
+  ps auxwf | grep -v grep | grep --color $1
+}
+alias psg=psgFunction
+
+# Weather service
+alias wpraha="wttr Prague"
+alias wtrutnov="wttr Trutnov"
---
+++ custom/path.bash
@@ -0,0 +1,1 @@
+export PATH="$PATH:$HOME/bin:/opt/PhpStorm/bin:./vendor/bin:./node_modules/.bin:/opt/apache-maven-3.3.9/bin"
